466d03a65bddaf4ff086f0a2ee1b67a4c9b4ab35
MuhammadAlfariziT <muhammad.tazkia@tiket.com>
2022-07-09 17:50:03 +0700

    [feat] add models file

---
+++ app/Models/KelasDanMember.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KelasDanMember extends Model
+{
+    use HasFactory;
+
+    protected $table = "kelas_dan_member";
+}
---
+++ app/Models/SiswaDanSoalUjian.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SiswaDanSoalUjian extends Model
+{
+    use HasFactory;
+
+    protected $table = "siswa_dan_soal_ujian";
+}
