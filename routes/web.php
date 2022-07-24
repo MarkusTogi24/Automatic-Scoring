103735ef5e23b65e88ba882b006dd982ff12c66f
MuhammadAlfariziT <muhammad.tazkia@tiket.com>
2022-07-24 14:36:45 +0700

    add routes for quesiton

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,15 @@ Route::get('/classroom/{id}/exams', [ExamController::class, 'show']);
 Route::post('/classroom/exam-create', [ExamController::class, 'store']);
 Route::get('/classroom/exam-update/{id}', [ExamController::class, 'edit']);
 Route::put('/classroom/exam-update/{id}', [ExamController::class, 'update']);
-Route::get('/deleteexam/{id}', [ExamController::class, 'destroy']);
+Route::delete('/classroom/{classroom_id}/exam-delete/{exam_id}', [ExamController::class, 'destroy']);
+
+Route::get('/classroom/{classroom_id}/exam-show-question/{exam_id}/question/{question_id}', [QuestionController::class, 'show']);
+Route::get('/classroom/{classroom_id}/exam-show-all-question/{exam_id}', [QuestionController::class, 'index']);
+Route::get('/classroom/{classroom_id}/exam-add-question/{exam_id}', [QuestionController::class, 'create']);
+Route::post('/classroom/{classroom_id}/exam-add-question/{exam_id}', [QuestionController::class, 'store']);
+Route::get('/classroom/{classroom_id}/exam-edit-question/{question_id}', [QuestionController::class, 'edit']);
+Route::put('/classroom/{classroom_id}/exam-edit-question/{question_id}', [QuestionController::class, 'update']);
+Route::delete('/classroom/{classroom_id}/exam-delete-question/{question_id}', [QuestionController::class, 'destroy']);
+
 Route::resource('/question', QuestionController::class);
 Route::resource('/member', ClassroomAndMemberController::class);
