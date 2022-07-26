d9349835ef63bfc39d0b2331163242c5d4162e63
MuhammadAlfariziT <muhammad.tazkia@tiket.com>
2022-07-26 21:13:01 +0700

    rapihin routes

--- routes/web.php
+++ routes/web.php
@@ -25,21 +25,48 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route for CRUD classroom
 Route::resource('/classroom', ClassroomController::class);
-Route::get('/classroom/{id}/exam-create', [ExamController::class, 'create']);
-Route::get('/classroom/{id}/exams', [ExamController::class, 'show']);
-Route::post('/classroom/exam-create', [ExamController::class, 'store']);
-Route::get('/classroom/exam-update/{id}', [ExamController::class, 'edit']);
-Route::put('/classroom/exam-update/{id}', [ExamController::class, 'update']);
+
+// Route for see member of classroom
+Route::get('/classroom/{classroom_id}/members', [ClassroomAndMemberController::class, 'index']);
+
+// Route for create new exam
+Route::get('/classroom/{classroom_id}/exam-create', [ExamController::class, 'create']);
+Route::post('/classroom/{classroom_id}/exam-create', [ExamController::class, 'store']);
+
+// Route for show all exam of the classroom
+Route::get('/classroom/{classroom_id}/exam-show-all', [ExamController::class, 'index']);
+
+// Route for show one exam by exam id
+Route::get('/classroom/{classroom_id}/exam/{exam_id}', [ExamController::class, 'show']);
+
+// Route for updating exam information
+Route::get('/classroom/{classroom_id}/exam-update/{exam_id}', [ExamController::class, 'edit']);
+Route::put('/classroom/{classroom_id}/exam-update/{exam_id}', [ExamController::class, 'update']);
+
+// Route for delete exam entity
 Route::delete('/classroom/{classroom_id}/exam-delete/{exam_id}', [ExamController::class, 'destroy']);
 
-Route::get('/classroom/{classroom_id}/exam-show-question/{exam_id}/question/{question_id}', [QuestionController::class, 'show']);
+// Route for open or close exam
+Route::post('/classroom/{classroom_id}/exam-change-status/{exam_id}', [ExamController::class, 'changeStatus']);
+
+// Route for show question (all and by question id)
+Route::get('/classroom/{classroom_id}/exam-show-question_by_id/{question_id}', [QuestionController::class, 'show']);
 Route::get('/classroom/{classroom_id}/exam-show-all-question/{exam_id}', [QuestionController::class, 'index']);
+Route::get('/classroom/{classroom_id}/start-exam/{exam_id}', [QuestionController::class, 'startExam']);
+
+// Route for add question of the exam
 Route::get('/classroom/{classroom_id}/exam-add-question/{exam_id}', [QuestionController::class, 'create']);
 Route::post('/classroom/{classroom_id}/exam-add-question/{exam_id}', [QuestionController::class, 'store']);
+
+// Route for update question of the exam
 Route::get('/classroom/{classroom_id}/exam-edit-question/{question_id}', [QuestionController::class, 'edit']);
 Route::put('/classroom/{classroom_id}/exam-edit-question/{question_id}', [QuestionController::class, 'update']);
-Route::delete('/classroom/{classroom_id}/exam-delete-question/{question_id}', [QuestionController::class, 'destroy']);
+
+// Route for delete exam question
+Route::get('/classroom/{classroom_id}/exam-delete-question/{question_id}', [QuestionController::class, 'destroy']);
 
 Route::resource('/question', QuestionController::class);
 Route::resource('/member', ClassroomAndMemberController::class);
