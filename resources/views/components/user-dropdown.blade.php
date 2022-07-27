9022fa0771708f9ce17b3cbd91f0b77c5a9dba20
Togi Stark <markus.118140037@student.itera.ac.id>
2022-07-27 22:19:29 +0700

    SideBar Static Home Page

---
+++ resources/views/components/user-dropdown.blade.php
@@ -0,0 +1,96 @@
+<div class="relative">
+    <!-- USER AUTH ACTIONS DROPDOWN BUTTON -->
+    <button class="flex items-center gap-2"
+        x-on:click="showAuthMenu =! showAuthMenu"
+    >
+        <img src="{{ asset('image/dummy_pp.png') }}" class="w-9 h-9" alt="">
+        <p class="hidden lg:inline-block text-sm">{{ Auth::user()->name }}</p>
+        <span class="hidden lg:inline-block" x-bind:class=" showAuthMenu && 'rotate-180' ">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+            </svg>
+        </span>
+    </button>
+
+    <!-- USER AUTH ACTIONS DROPDOWN MENU -->
+    <div class="absolute top-full mt-4 right-0 bg-white z-[1000] text-gray-700 w-40 flex flex-col gap-2.5 py-2 rounded-b-md drop-shadow-md"
+        x-cloak x-show="showAuthMenu"
+        x-on:click.outside="showAuthMenu = false"
+        x-transition:enter.duration.300ms
+        x-transition:leave.duration.300ms>
+        <div class="px-3 lg:hidden">
+            <p class="text-sm text-primary-80 font-semibold">{{ Auth::user()->name }}</p>
+        </div>
+
+        <!-- SEARCH INPUT TRIGGER -->
+        <div class="px-3 lg:hidden">
+            <button type="button" class="text-sm flex gap-2 items-center w-full py-1 px-2 rounded border-2 border-primary-10 text-primary-50 bg-primary-10"
+                x-on:click="
+                    showAuthMenu = false,
+                    showSearchInput = true
+                ">
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </span>
+                <span class="font-semibold">
+                    {{ __('Cari') }}
+                </span>
+            </button>
+        </div>
+        
+        <!-- LINK TO PROFILE PAGE -->
+        <div class="px-3">
+            <a href="#" role="button" class="text-sm flex gap-2 items-center py-1 px-2 rounded border-2 border-primary bg-white text-primary hover:bg-primary hover:text-white">
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                    </svg>
+                </span>
+                <span class="font-semibold">
+                    {{ __('Profil') }}
+                </span>
+            </a>
+        </div>
+
+        <div class="border-t-2 border-gray-200"></div>
+
+        <!-- LOGOUT BUTTON -->
+        <div class="px-3">
+            <form action="{{ route('logout') }}" method="POST">
+                <button type="submit" class="text-sm flex gap-2 items-center w-full py-1 px-2 rounded border-2 border-danger bg-white text-danger hover:bg-danger hover:text-white">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                    <span class="font-semibold">
+                        {{ __('Logout') }}
+                    </span>
+                </button>
+            </form>
+        </div>
+    </div>
+
+    <!-- MOBILE SEARCH INPUT -->
+    <div class="absolute top-full mt-4 right-0 w-[calc(100vw-2rem)] bg-white rounded-b-md drop-shadow-md p-2"
+        x-cloak x-show="showSearchInput"
+        x-transition:enter.duration.300ms
+        x-transition:leave.duration.300ms
+    >
+        <input class="block border border-primary-20 text-primary-50 text-sm rounded-md focus:ring-primary-70 focus:border-primary-70 w-full p-1.5 px-8 custom-placeholder" 
+            type="text" name="search" id="search" placeholder="Cari">
+        <span class="absolute inset-y-2 left-2 z-[1000] w-8 p-1.5 aspect-square flex items-center text-primary-50">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+        </span>
+        <button type="button" class="absolute inset-y-[9px] right-[9px] z-[1000] w-8 p-1.5 aspect-square flex items-center rounded-r bg-white text-gray-300"
+            x-on:click=" showSearchInput = false ">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+            </svg>
+        </button>
+    </div>
+</div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,19 +17,26 @@
     <!-- Main Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <nav class="fixed z-[999] top-0 w-full flex justify-between p-4 bg-primary-50 text-white">
+<body 
+    x-data="{
+        showSidebar     : false,
+        showAuthMenu    : false,
+        showSearchInput : false,
+        showNotifMenu   : false,
+    }">
+    <nav class="fixed z-[999] top-0 w-full flex justify-between p-4 lg:px-6 bg-primary-50 text-white">
 
-        {{-- <div class="flex justify-between items-center border border-black w-[256px]"> --}}
-        <div class="flex justify-between items-center  w-[256px]">
-            {{-- <a href="" class="flex gap-2 items-center border border-black"> --}}
-            <a href="" class="hidden lg:flex gap-2 items-center ">
-                <img src="{{ asset('image/logo_tut_wuri.png') }}" class="w-9 h-9" alt="">
-                {{-- <p class="text-base font-medium border border-black">SMAN 123 RAJAWALI</p> --}}
+        <!-- LOGO AND SIDEBAR TOGGLER -->
+        <div class="flex justify-between items-center w-[256px]">
+            <a href="" class="gap-2 items-center "
+                x-cloak 
+                x-bind:class="showSidebar ? 'hidden lg:flex' : 'hidden'"
+                x-transition:enter.duration.400ms
+                x-transition:leave.duration.400ms>
+                <img src="{{ asset('image/logo_tut_wuri.png') }}" class="w-9 h-9" alt="LOGO">
                 <p class="text-base font-medium ">SMAN 123 RAJAWALI</p>
             </a>
-            {{-- <button class="block border border-black"> --}}
-            <button class="block ">
+            <button type="button" class="block" x-on:click="showSidebar =! showSidebar">
                 <span>
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
@@ -38,8 +45,9 @@
             </button>
         </div>
 
+        <!-- HEADER ACTION GROUP SECTION -->
         <div class="flex gap-4 items-center">
-            {{-- <div class="w-72 relative border border-black"> --}}
+            <!-- SEARCH INPUT -->
             <div class="hidden md:block w-64 lg:w-72 relative ">
                 <input class="block border border-primary-20 text-primary-50 text-sm rounded-md focus:ring-primary-70 focus:border-primary-70 w-full p-1.5 pl-8 custom-placeholder" 
                     type="text" name="search" id="search" placeholder="Cari">
@@ -50,11 +58,9 @@
                 </span>
             </div>
             
-            <!-- NOTICATION -->
-            {{-- <div class="relative border border-black"> --}}
-            <div class="relative ">
-                {{-- <button class="flex items-center border border-black"> --}}
-                <button class="flex items-center ">
+            <!-- USER NOTIFICATION -->
+            <div class="relative">
+                <button class="flex items-center">
                     <span>
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
@@ -62,63 +68,144 @@
                     </span>
                 </button>
             </div>
-            {{-- <button class="flex items-center gap-2 border border-black"> --}}
-            <button class="flex items-center gap-2 ">
-                <img src="{{ asset('image/dummy_pp.png') }}" class="w-9 h-9" alt="">
-                <p class="hidden lg:inline-block text-sm">{{ Auth::user()->name }}</p>
-                <span class="hidden lg:inline-block ">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-                    </svg>
-                </span>
-            </button>
-            {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
 
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div> --}}
+            <!-- USER AUTH ACTIONS DROPDOWN -->
+            @include('components.user-dropdown')
         </div>
     </nav>
 
-    <main class="py-4">
-        @yield('content')
+    <main class="relative w-full overflow-x-hidden">
+        <!-- SIDEBAR -->
+        <aside class="bg-primary-10 fixed z-[1000] top-[4.25rem] h-[calc(100vh-4.25rem)] p-3 flex flex-col gap-4"
+            x-cloak
+            x-bind:class="showSidebar ? 'left-0 w-[288px] lg:w-[304px]' : 'right-full lg:left-0 w-fit ' "
+            x-transition:enter.duration.400ms
+            x-transition:leave.duration.400ms
+            x-data="{
+                openSubjectCollapse     : false,
+                openDataMasterCollapse  : false
+            }">
+
+            <!-- LOGO FOR SMALL SCREEN -->
+            <a href="" class="lg:hidden flex gap-2 items-center py-3">
+                <img src="{{ asset('image/logo_tut_wuri.png') }}" class="w-9 h-9" alt="">
+                <p class="text-base font-medium ">SMAN 123 RAJAWALI</p>
+            </a>
+
+            <!-- HOME -->
+            <a href="" class="text-white bg-primary p-3 rounded-md" x-bind:class="showSidebar ? 'w-full' : 'w-fit' ">
+                <div class="flex gap-3 items-center" x-bind:class="showSidebar ? 'w-full' : 'w-fit' ">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                        </svg>
+                    </span>
+                    <span x-bind:class="showSidebar ? 'inline-block' : 'hidden' ">
+                        Beranda
+                    </span>
+                </div>
+            </a>
+
+            <!-- SUBJECTS COLLAPSE BUTTON -->
+            <button x-on:click="openSubjectCollapse =! openSubjectCollapse" class="flex justify-between items-center text-white bg-primary p-3 rounded-md"  x-bind:class="showSidebar ? 'w-full' : 'w-fit' ">
+                <div class="flex gap-3 items-center" x-bind:class="showSidebar ? 'w-full' : 'w-fit' ">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                        </svg>
+                    </span>
+                    <span x-bind:class="showSidebar ? 'inline-block' : 'hidden' ">
+                        Mata Pelajaran
+                    </span>
+                </div>
+                <span x-bind:class="showSidebar ? 'inline-block' : 'hidden' ">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" x-bind:class="openSubjectCollapse && 'rotate-90' " viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                    </svg>
+                </span>
+            </button>
+
+            <!-- SUBJECTS COLLAPSE MENU -->
+            <div class="flex flex-col gap-3 px-3"
+                x-cloak x-collapse 
+                x-show="openSubjectCollapse && showSidebar"
+                x-transition:enter.duration.700ms
+                x-transition:leave.duration.350ms
+                >
+                <a href="" class="text-primary-90 font-thin py-1">
+                    Bahasa Indonesia
+                </a>
+                <a href="" class="text-primary-90 font-thin py-1">
+                    Matematika
+                </a>
+                <a href="" class="text-primary-90 font-thin py-1">
+                    Bahasa Inggris
+                </a>
+                <a href="" class="text-primary-90 font-thin py-1">
+                    Ilmu Pengetahuan Alam
+                </a>
+                <a href="" class="text-primary-90 font-thin py-1">
+                    Ilmu Pengetahuan Sosial
+                </a>
+            </div>
+
+            <!-- DATA MASTER COLLAPSE BUTTON FOR ADMIN -->
+            <button x-on:click="openDataMasterCollapse =! openDataMasterCollapse" 
+                class="flex justify-between items-center text-white bg-primary p-3 rounded-md" 
+                x-bind:class="showSidebar ? 'w-full' : 'w-fit' ">
+                <div class="flex gap-3 items-center" x-bind:class="showSidebar ? 'w-full' : 'w-fit' ">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
+                        </svg>
+                    </span>
+                    <span x-bind:class="showSidebar ? 'inline-block' : 'hidden' ">
+                        Data Master
+                    </span>
+                </div>
+                <span x-bind:class="showSidebar ? 'inline-block' : 'hidden' ">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" x-bind:class="openDataMasterCollapse && 'rotate-90'" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                    </svg>
+                </span>
+            </button>
+
+            <!-- DATA MASTER COLLAPSE MENU FOR ADMIN -->
+            <div class="flex flex-col gap-3 px-3"
+                x-cloak x-collapse
+                x-show="openDataMasterCollapse && showSidebar" 
+                x-transition:enter.duration.300ms
+                x-transition:leave.duration.300ms
+                >
+                <a href="" class="text-primary-90 font-thin py-1">
+                    Data Akun
+                </a>
+                <a href="" class="text-primary-90 font-thin py-1">
+                    Data Kelas
+                </a>
+            </div>
+
+            <!-- USER PROFILE -->
+            <a href="" class="text-white bg-primary p-3 rounded-md" x-bind:class="showSidebar ? 'w-full' : 'w-fit' ">
+                <div class="flex gap-3 items-center" x-bind:class="showSidebar ? 'w-full' : 'w-fit' ">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </span>
+                    <span x-bind:class="showSidebar ? 'inline-block' : 'hidden'">
+                        Profil Saya
+                    </span>
+                </div>
+            </a>
+        </aside>
+        
+        <div class="fixed z-[500] w-full bg-white top-[4.25rem] right-0 min-h-[calc(100vh-4.25rem)] p-4"
+            x-bind:class="showSidebar ? 'lg:w-[calc(100vw-304px)]' : 'lg:w-[calc(100vw-4.25rem)]' "
+            >
+            {{-- @yield('alertMsgContainer') --}}
+            @yield('content')
+        </div>
     </main>
 
     @yield('scripts')
