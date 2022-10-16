e0f785f17008d68a82a21e699bb20b7acc6b07ac
Muhammad Alfarizi Tazkia <103025765+MuhammadAlfariziT@users.noreply.github.com>
2022-10-17 00:12:16 +0700

    Kalo udah ditutup gausah nyimpen jawaban lagi

--- app/Http/Controllers/StudentAndQuestionController.php
+++ app/Http/Controllers/StudentAndQuestionController.php
@@ -25,25 +25,17 @@ class StudentAndQuestionController extends Controller
     {
         //
     }
-    
+
     public function create()
     {
         //
     }
-    
+
     public function store(Request $request, Exam $exam)
     {
         $updated_exam_info = Exam::find($exam->id);
 
-        $entity = new StudentAndQuestion;
-
-        $entity->question_id    = $request->question_id;
-        $entity->answer         = $request->answer;
-        $entity->student_id     = Auth::user()->id;
-        $entity->score          = 0;
-        $entity->save();
-        
-        if ($updated_exam_info->is_open == 0){
+        if ($updated_exam_info->is_open == 0) {
             $this->helper->save_total_score($exam->id);
             $data = [
                 "exam_status"   => "closed",
@@ -52,68 +44,77 @@ class StudentAndQuestionController extends Controller
             ];
             return Response::json($data);
         } else {
+
+            $entity = new StudentAndQuestion;
+    
+            $entity->question_id    = $request->question_id;
+            $entity->answer         = $request->answer;
+            $entity->student_id     = Auth::user()->id;
+            $entity->score          = 0;
+            $entity->save();
+
             $questions = Question::select("question.*", "student_and_question.answer as answer", "student_and_question.id as answer_id")
                 ->leftJoin('student_and_question', function ($join) {
                     $join->on('question.id', 'student_and_question.question_id');
                     $join->on('student_and_question.student_id', DB::raw(Auth::user()->id));
-                })                   
+                })
                 ->where('question.exam_id', $exam->id)
                 ->orderBy('id')
                 ->get();
             return Response::json($questions);
         }
-
     }
-    
+
     public function show($id)
     {
         //
     }
-    
+
     public function edit($id)
     {
         //
     }
-    
+
     public function update(Request $request, Exam $exam)
     {
         $updated_exam_info = Exam::find($exam->id);
-        
-        $entity = StudentAndQuestion::firstWhere('id', $request->id);
 
-        $entity->question_id    = $request->question_id;
-        $entity->answer         = $request->answer;
-        $entity->student_id     = Auth::user()->id;
-        $entity->score          = 0;
-        $entity->save();
-        
-        if ($updated_exam_info->is_open == 0){
+        if ($updated_exam_info->is_open == 0) {
             $this->helper->save_total_score($exam->id);
+            
             $data = [
                 "exam_status"   => "closed",
                 "classroom_id"  => $exam->class_id,
                 "exam_id"       => $exam->id
             ];
             return Response::json($data);
-        }else {
+        } else {
+            $entity = StudentAndQuestion::firstWhere('id', $request->id);
+
+            $entity->question_id    = $request->question_id;
+            $entity->answer         = $request->answer;
+            $entity->student_id     = Auth::user()->id;
+            $entity->score          = 0;
+            $entity->save();
             $questions = Question::select("question.*", "student_and_question.answer as answer", "student_and_question.id as answer_id")
                 ->leftJoin('student_and_question', function ($join) {
                     $join->on('question.id', 'student_and_question.question_id');
                     $join->on('student_and_question.student_id', DB::raw(Auth::user()->id));
-                })                   
+                })
                 ->where('question.exam_id', $exam->id)
                 ->orderBy('id')
                 ->get();
             return Response::json($questions);
         }
     }
-    
+
     public function destroy($id)
     {
         //
     }
 
-    public function submit(Exam $exam) {
+    public function submit(Exam $exam)
+    {
         $this->helper->save_total_score($exam->id);
     }
 }
